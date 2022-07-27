d33ccd68f66862b13c8840b3aed4deef58e80421
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-07-27 15:19:20 +0200

    Architecture du projet

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <header>
+      <h1>Footer</h1>
+    </header>
+  );
+}
+
+export default Footer;
