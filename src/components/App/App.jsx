d25c1e6a1036ef905643dd017e1ec7ecf3a53a3c
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-07-27 15:49:27 +0200

    Mise en place du router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Home from "../../pages/Home";
+import About from "../../pages/About";
+import Error404 from "../../pages/Error404";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Error404.jsx
@@ -0,0 +1,8 @@
+function Error404() {
+  return (
+    <div>
+      <h1>Page - Error 404</h1>
+    </div>
+  );
+}
+export default Error404;
