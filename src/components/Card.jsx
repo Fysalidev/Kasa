c738deefccf0533c6774914948a67f642f6d8e40
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-07-29 10:25:15 +0200

    Page Home

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import '../styles/Card.css';
+
+function Card({ title }) {
+  return (
+    <div className="accomodation_card">
+      <h2>{title}</h2>
+    </div>
+  );
+}
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import data from '../data/data.json';
+import "../styles/Home.css";
+import data from "../data/data.json";
 import Banner from "../components/Banner";
-import{Link} from 'react-router-dom';
-
-const accomodations = data.map((accomodation) => accomodation)
-console.log(accomodations);
+import { Link } from "react-router-dom";
+import Card from "../components/Card";
 
 function Home() {
+  const accomodations = data.map((accomodation) => accomodation);
+
   return (
     <section className="home">
       <Banner title="Chez vous, partout et ailleurs" type="banner-home" />
@@ -14,12 +15,11 @@ function Home() {
           {accomodations.map((accomodation) => (
             <li key={accomodation.id}>
               <Link to={`/accomodation/${accomodation.id}`}>
-                {accomodation.title}
+                <Card title={accomodation.title} />
               </Link>
             </li>
           ))}
         </ul>
-
       </div>
     </section>
   );
