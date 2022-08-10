0f0cd99679b9d1ce666a7eb9208cc18beddbd124
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-08-10 22:07:48 +0200

    Mardi soir

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,17 +12,15 @@ function Home({ accomodations }) {
   return (
     <section className="home">
       <Banner title="Chez vous, partout et ailleurs" type="banner-home" />
-      <div className="gallery">
-        <ul>
-          {accomodations.map((accomodation) => (
-            <li key={accomodation.id}>
-              <Link to={`/accomodation/${accomodation.id}`}>
-                <Card title={accomodation.title} cover={accomodation.cover} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul className="accomodation-list">
+        {accomodations.map((accomodation) => (
+          <li key={accomodation.id}>
+            <Link to={`/accomodation/${accomodation.id}`}>
+              <Card title={accomodation.title} cover={accomodation.cover} />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
